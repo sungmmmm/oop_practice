fa8824f2ef4fb936d653c1832a55536fa8fbec13
sungmmmm <sadlzn77@gmail.com>
2023-07-14 20:43:24 +0900

    PasswordVaildator (1)

--- src/main/java/org/example/PasswordVaildator.java
+++ src/main/java/org/example/PasswordVaildator.java
@@ -1,2 +1,12 @@
-package org.example;public class PasswordVaildator {
+package org.example;
+
+public class PasswordVaildator {
+    public static final String WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAG = "비밀번호는 최소 8자 이상 12자 이하여야 한다.";
+
+    public static void validate(String password) {
+        //3.테스트 코드가 통과해서 해당 부분을 리펙토링 해주기
+        if (password.length() < 8 || password.length() > 12) {
+            throw new IllegalArgumentException(WRONG_PASSWORD_LENGTH_EXCEPTION_MESSAG);//command+option+c 로 해서 리펙토링
+        }
+    }
 }
--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -1,2 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+/**
+ * - **요구사항**
+ *     - 비밀번호는 최소 8자 이상 12자 이하여야 한다.
+ *     - 비밀번호가 8자 미만 또는 12자 초과인 경우 IllegalArgumentException 예외를 발생
+ *     시킨다.
+ *     - 경계조건에 대해 테스트 코드를 작성해야 한다.
+ */
 public class PasswordValidatorTest {
+    @DisplayName("비밀번호가 최소 8자 이상 , 12자 이하면 예외가 발생하지 않는다")
+    @Test //command + n (test Method)에서 생성
+    void validatePasswordTest() {
+        assertThatCode(() -> PasswordVaildator.validate("serverwizard"))
+                .doesNotThrowAnyException(); //예외 발생하지 않는다 라는걸 확인하기 위해서
+    }
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(() -> PasswordVaildator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class)//assertj 안에 있는 코드임
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
 }
