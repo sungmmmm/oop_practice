eb4f5ed79dd1a57086171ce0463d51714e29b582
sungmmmm <sadlzn77@gmail.com>
2023-07-15 19:43:05 +0900

    테스트 코드의 낮은 결합 예

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,15 +3,21 @@ package org.example;
 public class User { //command + n 누르고 test UserTest 만들기
     private String password;
 
-    public void initPassword() {
-        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
-        String randomPassword = randomPasswordGenerator.generatePassword();
+    public void initPassword(PasswordGenerator PasswordGenerator) {
+        //컨트롤 할 수 없는 부분을(RandomPasswordGenerator) 외부로(PasswordGenerator)부터 주입 받기 위해서 추가하기
+        //RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator();
+        String password =PasswordGenerator.generatePassword();
         /*
            비밀번호는 최소 8자 이상 12자 이하여야한다.
          */
-        if(randomPassword.length() >= 8 && randomPassword.length() <= 12){
-        this.password = randomPassword;
+        if(password.length() >= 8 && password.length() <= 12){
+        this.password = password;
         }
 
+    }
+
+    public String getPassword() {
+        //command + n getter생성
+        return password;
     }
 }
--- src/test/java/org/example/UserTest.java
+++ src/test/java/org/example/UserTest.java
@@ -1,12 +1,36 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+
+    @DisplayName("패스워드를 초기화 한다.")
     @Test //command + n testMethod
-    void name() {
+    void passwordTest() {
+        // given
+        User user = new User();
+        // when 이걸 호출했을때
+        user.initPassword(new CorrectFixedPasswordGenerator());
+        //then
+        assertThat(user.getPassword()).isNotNull();
+        //option+enter 해서 assertj로 변경
+
+    }
+
+    @DisplayName("패스워드가 요구사항에 부합하지 않아 초기화가 되지 않는다.")
+    @Test //command + n testMethod
+    void passwordTest2() {
+        // given
+        User user = new User();
+        // when 이걸 호출했을때
+        user.initPassword(new WrongFixedPasswordGenerator());
+        //then
+        assertThat(user.getPassword()).isNull();
+        //option+enter 해서 assertj로 변경
 
     }
 }
